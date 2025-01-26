2d1dd3df8c77a8a40fb3d1ef3e848a72dde3ddc3
MAHESH <maheshshinde9100@gmail.com>
2025-01-26 20:56:56 +0530

    working 100%

--- practice_codes/singlyLL.cpp
+++ practice_codes/singlyLL.cpp
@@ -1,89 +1,130 @@
 #include <iostream>
 using namespace std;
-class Node{
+class Node {
 public:
     int data;
     Node *next;
-    Node(int val){
+    Node(int val) {
         data = val;
         next = nullptr;
     }
-}*headd, *temp, *indexx;
-void insertAtFirst(Node *head, int val){
+}*temp,*indexx,*head = nullptr;
+
+void insertAtFirst(int val) {
     temp = new Node(val);
-    if (head == nullptr){
+    if (head == nullptr) {
+        head = temp;
+    } else {
+        temp->next = head;
         head = temp;
     }
-    else{
-        head->next = temp;
-    }
 }
-void insertAtLast(Node *head, int val){
+
+void insertAtLast(int val) {
     temp = new Node(val);
-    if (head == nullptr){
+    if (head == nullptr) {
         head = temp;
-    }
-    else{
+    } else {
         indexx = head;
-        while (indexx->next->next != nullptr){
+        while (indexx->next != nullptr) {
             indexx = indexx->next;
         }
-        indexx->next->next = temp;
-        temp->next = nullptr;
+        indexx->next = temp;
     }
 }
-void insertAtSpecificLoc(Node *head, int i){
-    int check = 0;
-    if (head == nullptr){
-        cout << "Can't insert at index " << i << endl;
+
+void insertAtSpecificLoc(int i) {
+    if (head == nullptr) {
+        cout << "Can't insert at indexx " << i << endl;
+        return;
     }
-    else{
-        int count = 0;
-        indexx = head;
-        while (indexx->next != nullptr){
-            if (count == i){
-                int val;
-                cout << "Enter value :" << endl;
-                cin >> val;
-                Node *newNode = new Node(val);
-                temp = indexx->next;
-                indexx->next = newNode;
-                newNode->next = temp;
-                check = 1;
-                break;
-            }
-            count++;
-            indexx = indexx->next;
-        }
+    int count = 0;
+    indexx = head;
+    while (indexx != nullptr && count < i - 1) {
+        indexx = indexx->next;
+        count++;
     }
-    if (check == 1){
-        cout << "Inserted succesfully...!!" << endl;
+    if (indexx == nullptr) {
+        cout << "indexx out of bounds" << endl;
+        return;
     }
-    else{
-        cout << "Something went wrong...!!" << endl;
-    }
+    int val;
+    cout << "Enter value :" << endl;
+    cin >> val;
+    Node *newNode = new Node(val);
+    newNode->next = indexx->next;
+    indexx->next = newNode;
+    cout << "Inserted successfully...!!" << endl;
 }
-void deleteAtFirst(Node *head){
-}
-void deleteAtLast(Node *head){
-}
-void deleteAtSpecificLoc(Node *head){
-}
-void displayLL(Node *head){
-    if (head == nullptr){
+
+void deleteAtFirst() {
+    if (head == nullptr) {
         cout << "Linked list is empty" << endl;
-    }else{
-        indexx = head;
-        while (indexx->next != nullptr){
-            cout << " " << indexx->data << "-> ";
-            indexx = indexx->next;
-        }
+        return;
     }
+    temp = head;
+    head = head->next;
+    delete temp;
 }
-int main(){
+
+void deleteAtLast() {
+    if (head == nullptr) {
+        cout << "Linked list is empty" << endl;
+        return;
+    }
+    if (head->next == nullptr) {
+        delete head;
+        head = nullptr;
+        return;
+    }
+    indexx = head;
+    while (indexx->next->next != nullptr) {
+        indexx = indexx->next;
+    }
+    delete indexx->next;
+    indexx->next = nullptr;
+}
+
+void deleteAtSpecificLoc(int i) {
+    if (head == nullptr) {
+        cout << "Linked list is empty" << endl;
+        return;
+    }
+    if (i == 0) {
+        deleteAtFirst();
+        return;
+    }
+    int count = 0;
+    indexx = head;
+    while (indexx != nullptr && count < i - 1) {
+        indexx = indexx->next;
+        count++;
+    }
+    if (indexx == nullptr || indexx->next == nullptr) {
+        cout << "indexx out of bounds" << endl;
+        return;
+    }
+    temp = indexx->next;
+    indexx->next = temp->next;
+    delete temp;
+}
+
+void displayLL() {
+    if (head == nullptr) {
+        cout << "Linked list is empty" << endl;
+        return;
+    }
+    indexx = head;
+    while (indexx != nullptr) {
+        cout << indexx->data << " -> ";
+        indexx = indexx->next;
+    }
+    cout << "nullptr" << endl;
+}
+
+int main() {
     int val, ch, c, i;
-    headd = nullptr;
-    do{
+    do {
         cout << "Operations Menu :>" << endl;
         cout << "1. Insert at first" << endl;
         cout << "2. Insert at last" << endl;
@@ -93,38 +134,40 @@ int main(){
         cout << "6. Delete at specific location" << endl;
         cout << "7. Display Linked List" << endl;
         cin >> ch;
-        switch (ch){
+        switch (ch) {
         case 1:
             cout << "Enter value :" << endl;
             cin >> val;
-            insertAtFirst(headd, val);
+            insertAtFirst(val);
             break;
         case 2:
             cout << "Enter value :" << endl;
             cin >> val;
-            insertAtLast(headd, val);
+            insertAtLast(val);
             break;
         case 3:
-            cout << "Enter index :" << endl;
+            cout << "Enter indexx :" << endl;
             cin >> i;
-            insertAtSpecificLoc(headd, i);
+            insertAtSpecificLoc(i);
             break;
         case 4:
-            deleteAtFirst(headd);
+            deleteAtFirst();
             break;
         case 5:
-            deleteAtLast(headd);
+            deleteAtLast();
             break;
         case 6:
-            deleteAtSpecificLoc(headd);
+            cout << "Enter indexx :" << endl;
+            cin >> i;
+            deleteAtSpecificLoc(i);
             break;
         case 7:
-            displayLL(headd);
+            displayLL();
             break;
         default:
             cout << "Invalid Option Selection!!!!!" << endl;
         }
-        cout << "DO you want to continue ? is yes(1)" << endl;
+        cout << "Do you want to continue? If yes, enter 1: ";
         cin >> c;
     } while (c == 1);
     return 0;
