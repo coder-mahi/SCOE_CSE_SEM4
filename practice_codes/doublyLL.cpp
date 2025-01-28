e9ec70d167d22a90aad4ba7b771af57de0d0e66a
MAHESH <maheshshinde9100@gmail.com>
2025-01-28 20:53:09 +0530

    doubly LL

--- practice_codes/doublyLL.cpp
+++ practice_codes/doublyLL.cpp
@@ -1,59 +1,141 @@
-#include<iostream>
-struct Node{
+#include <iostream>
+using namespace std;
+
+// Define Node structure
+struct Node {
     int data;
     Node *next;
     Node *prev;
-    Node(int val){
+
+    Node(int val) {
         data = val;
         next = nullptr;
         prev = nullptr;
     }
-}*head,*temp,*indexx;
+};
 
-int main(){
-    int c,ch;
-    head = nullptr;
-    do{
-        cout<<"Choose Option :>"<<endl;
-        cout<<"1.insert"<<endl;
-        cout<<"2.delete"<<endl;
-        cout<<"3.display"<<endl;
-        cin>>ch;
-        switch (ch){
+// Declare global head pointer
+Node *head = nullptr;
+
+// Function to insert a node at a specific position
+void insert(int val, int pos) {
+    Node *newNode = new Node(val);
+
+    if (pos == 0) { // Insert at the beginning
+        newNode->next = head;
+        if (head != nullptr) {
+            head->prev = newNode;
+        }
+        head = newNode;
+        return;
+    }
+
+    Node *current = head;
+    int count = 0;
+
+    while (current != nullptr && count < pos - 1) {
+        current = current->next;
+        count++;
+    }
+
+    if (current == nullptr) {
+        cout << "Position out of bounds" << endl;
+        delete newNode;
+        return;
+    }
+
+    newNode->next = current->next;
+    newNode->prev = current;
+
+    if (current->next != nullptr) {
+        current->next->prev = newNode;
+    }
+
+    current->next = newNode;
+}
+
+// Function to delete a node at a specific position
+void deleteNode(int pos) {
+    if (head == nullptr) {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    if (pos == 0) { // Delete the first node
+        Node *temp = head;
+        head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        }
+        delete temp;
+        return;
+    }
+
+    Node *current = head;
+    int count = 0;
+
+    while (current != nullptr && count < pos) {
+        current = current->next;
+        count++;
+    }
+
+    if (current == nullptr) {
+        cout << "Position out of bounds" << endl;
+        return;
+    }
+
+    current->prev->next = current->next;
+
+    if (current->next != nullptr) {
+        current->next->prev = current->prev;
+    }
+
+    delete current;
+}
+
+// Function to display the list
+void display() {
+    Node *current = head;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+// Main function
+int main() {
+    int choice, data, pos, cont;
+    do {
+        cout << "Choose Option :" << endl;
+        cout << "1. Insert" << endl;
+        cout << "2. Delete" << endl;
+        cout << "3. Display" << endl;
+        cin >> choice;
+
+        switch (choice) {
         case 1:
-            int x,pos,count = 0;
-            cout<<"Enter data and At which pos you want to insert ? "<<endl;
-            cin>>x;
-            cin>>pos;
-            temp = new Node(x);
-            if(head==nullptr){
-                head->next = temp;
-            }else{
-            indexx = head;
-            while(indexx->next!=nullptr){
-                if(count==pos){
-                    break;
-                }
-                indexx = indexx->next;
-                count++;
-            }
-            Node *temp2 = indexx;
-            indexx->next = temp;
-            temp2->next = indexx;
-            indexx->prev = temp2;
-            }
-            
+            cout << "Enter data and position to insert: ";
+            cin >> data >> pos;
+            insert(data, pos);
             break;
         case 2:
+            cout << "Enter position to delete: ";
+            cin >> pos;
+            deleteNode(pos);
             break;
         case 3:
+            cout << "List: ";
+            display();
             break;
         default:
-            cout<<""<<endl;
+            cout << "Invalid choice" << endl;
             break;
         }
-        cout<<"Do you want to continue ? if yes 1"<<endl;
-        cin>>c;
-    }while(c==1);
+
+        cout << "Do you want to continue? (1 for Yes, 0 for No): ";
+        cin >> cont;
+    } while (cont == 1);
+
     return 0;
 }
