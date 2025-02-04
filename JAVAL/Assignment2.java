8824e9ecabcee5d7661efb6dab088a2e67072dfd
MAHESH <maheshshinde9100@gmail.com>
2025-02-04 14:32:55 +0530

    JAVAL -- assignment2

---
+++ JAVAL/Assignment2.java
@@ -0,0 +1,129 @@
+import java.util.*;
+
+abstract class Vehicle {
+    String brand;
+    String model;
+
+    public Vehicle(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+        System.out.println("Vehicle created : " + brand + " " + model);
+    }
+
+    public abstract void start();
+
+    public abstract void stop();
+
+    public final void vehicleInfo() {
+        System.out.println("vehicleof brand : " + brand + ", model: " + model);
+    }
+}
+
+class Car extends Vehicle {
+    public Car(String b, String m) {
+        super(b, m);
+    }
+
+    @Override
+    public void start() {
+        System.out.println(this.brand + "Car started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Car stopped");
+    }
+
+    public void showCarDetails() {
+        System.out.println("Car details: Brand -- " + this.brand + ", Model - " + this.model);
+    }
+}
+
+class Bike extends Vehicle {
+    public Bike(String b, String m) {
+        super(b, m);
+    }
+
+    @Override
+    public void start() {
+        System.out.println(this.brand + " Bike started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Bike stopped");
+    }
+
+    public void showBikeDetails() {
+        System.out.println("Bike details: Brand - " + this.brand + ", Model - " + this.model);
+    }
+}
+
+class Truck extends Vehicle {
+    public Truck(String brand, String model) {
+        super(brand, model);
+    }
+
+    @Override
+    public void start() {
+        System.out.println(this.brand + "Truck started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Truck stopped.....");
+    }
+
+    public void showTruckDetails() {
+        System.out.println("Truck detailss : Brand -- " + this.brand + ", Model - " + this.model);
+    }
+}
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ch, c;
+        do {
+            System.out.println("1.Car\n2.Bike\n3.Truck");
+            ch = scanner.nextInt();
+            switch (ch) {
+                case 1:
+                    System.out.print("Enter Car Brand: ");
+                    String carBrand = scanner.nextLine();
+                    System.out.print("Enter Car Model: ");
+                    String carModel = scanner.nextLine();
+                    Vehicle car = new Car(carBrand, carModel);
+                    car.start();
+                    car.stop();
+                    car.vehicleInfo();
+                    ((Car) car).showCarDetails();
+                    break;
+                case 2:
+                    System.out.print("Enter Bike Brand: ");
+                    String bikeBrand = scanner.nextLine();
+                    System.out.print("Enter Bike Model: ");
+                    String bikeModel = scanner.nextLine();
+                    Vehicle bike = new Bike(bikeBrand, bikeModel);
+                    bike.start();
+                    bike.stop();
+                    bike.vehicleInfo();
+                    ((Bike) bike).showBikeDetails();
+                    break;
+                case 3:
+                    System.out.print("Enter Truck Brand: ");
+                    String truckBrand = scanner.nextLine();
+                    System.out.print("Enter Truck Model: ");
+                    String truckModel = scanner.nextLine();
+                    Vehicle truck = new Truck(truckBrand, truckModel);
+                    truck.start();
+                    truck.stop();
+                    truck.vehicleInfo();
+                    ((Truck) truck).showTruckDetails();
+                    break;
+            }
+            System.out.println("Do you want to continue ? (1/0)");
+            c = scanner.nextInt();
+        } while (c == 1);
+        scanner.close();
+    }
+}
