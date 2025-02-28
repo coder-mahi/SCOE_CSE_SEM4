ff7fd6f095cd30973ea1a4424a4a8cf543b8ccc1
MAHESH <maheshshinde9100@gmail.com>
2025-02-28 06:34:59 +0530

    final DSL 02 all operations working correctly..

--- DSL/DSL__02.cpp
+++ DSL/DSL__02.cpp
@@ -97,24 +97,158 @@ void update(TreeNode*& root, int id, TreeNode* newNode) {
     }
 }
 
+void search(TreeNode* root, int x){
+    if(root == nullptr){
+        cout<<"Employee data Not found..."<<endl;
+        return;
+    }
+    if(x==root->id){
+        cout<<"Employee Data found.."<<endl;
+        return;
+    }
+    if(x<root->id){
+        search(root->left,x);
+    }else{
+        search(root->right,x);
+    }
+}
+
+
+int minSalary(TreeNode* root)
+{
+    if(root==nullptr){
+        cout<<"Tree is emptty"<<endl;
+        return -1;
+    }
+    int minSal;
+    if(root->left) minSal = min(minSal,minSalary(root->left));
+    if(root->right) minSal = min(minSal,minSalary(root->right));
+    return minSal;
+}
+
+int maxSalary(TreeNode* root)
+{
+    if(root==nullptr){
+        cout<<"Tree is emptty"<<endl;
+        return -1;
+    }
+    int maxSal;
+    if(root->left) maxSal = max(maxSal,maxSalary(root->left));
+    if(root->right) maxSal = max(maxSal,maxSalary(root->right));
+    return maxSal;
+}
+
+void findAvgSalary(TreeNode* root, int& count, int& total){
+    if(root==nullptr){
+        return;
+    }
+        findAvgSalary(root->left,count,total);
+        total = total+root->sal;
+        count++;
+        findAvgSalary(root->right, count, total);
+}
+
+int totalEmployees(TreeNode* root) 
+{
+    if(root==nullptr){
+        return 0;
+    }
+    return 1+totalEmployees(root->left) +totalEmployees(root->right);
+}
+
+void findJuniorEmployees(TreeNode* root,int x)
+{
+    if(root==nullptr) return;
+    if(x>root->id){  
+        display(root); 
+         findJuniorEmployees(root->right,x);
+    }
+    findJuniorEmployees(root->left,x);
+}
+
+
+void findseniorEmployees(TreeNode* root, int x)
+{
+    if(root == nullptr) return;
+    if(x<root->id) {  
+        display(root); 
+        findseniorEmployees(root->left,x);
+    }else {
+        findseniorEmployees(root->right,x);
+    }
+}
+
+
 int main() {
     root=nullptr;
     int id,newId,sal;
     string name;
-    create(root);
-    display(root);
-
-    cout<<"Enter id of emp whose details want to update ..."<<endl;
-    cin>>id;
-
-    cout<<"Enter updated id, name, salary :> "<<endl;
-    cin>>newId; 
-    cin>>name;
-    cin>>sal;
-
-    TreeNode* temp = new TreeNode(newId,name,sal);
-    update(root,id,temp);
-    cout<<"After updation --- :>"<<endl;
-    display(root);
+    int ch,c,x;
+    TreeNode* temp;
+    int count,total;
+    do{
+        cout<<"Select operation on BST -> "<<endl;
+        cout<<"1.create\n2.search\n3.update\n4.display\n5.minimum salary\n6.maximum salary\n7.find avg salary\n8.total no. of employee\n9.delete employee\n10.find junniors\n11.find seniors"<<endl;
+        cin>>ch;
+        switch(ch){
+            case 1:
+                create(root);
+                break;
+            case 2:
+                cout<<"Enter id of emp to search :> "<<endl;
+                cin>>x;
+                search(root,x);
+                break;
+            case 3:
+                cout<<"Enter id of emp whose details want to update ..."<<endl;
+                cin>>id;
+                cout<<"Enter updated id, name, salary :> "<<endl;
+                cin>>newId; 
+                cin>>name;
+                cin>>sal;
+                temp = new TreeNode(newId,name,sal);
+                update(root,id,temp);
+                break;
+            case 4:
+                display(root);
+                break;
+            case 5:
+                cout<<"\nMinimum salary :> "<<minSalary(root)<<endl;
+                break;
+            case 6:
+                cout<<"\nMaximum salary :> "<<maxSalary(root)<<endl;
+                break;
+            case 7:
+                count =0;
+                total =0;
+                findAvgSalary(root,count,total);
+                if(count>0){
+                    cout<<"avg salarry :>"<<(total/count)<<endl;
+                }else{
+                    cout<<"No employees found..!!!"<<endl;
+                }
+                break;
+            case 8:
+                cout<<"\nTOTAL EMP :> "<<totalEmployees(root);
+                break;
+            case 9:
+                cout<<"enter ID :> "<<endl;
+                cin>>id;
+                deleteNode(root,id);
+                break;
+            case 10:
+                cout<<"enter ID :> "<<endl;
+                cin>>id;
+                findJuniorEmployees(root,id);
+                break;
+            case 11:
+                cout<<"enter ID :> "<<endl;
+                cin>>id;
+                findseniorEmployees(root,id);
+                break;
+        }
+        cout<<"Do you want to continue ?"<<endl;
+        cin>>c;
+    }while(c==1);
     return 0;
 }
