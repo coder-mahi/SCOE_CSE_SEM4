67d18921bb33fe2f2a8a6be4be79e4067fa68cc3
MAHESH <maheshshinde9100@gmail.com>
2025-03-10 21:59:54 +0530

    --

--- DSL/PrimsAlgorithm_MST.cpp
+++ DSL/PrimsAlgorithm_MST.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int minKey(int key[], bool inMST[], int V) {
     int min = INF, minIndex = -1;
-    for (int v = 0; v < V; v++) {
-        if (!inMST[v] && key[v] < min) {
+    for(int v=0;v<V;v++){
+       if(!inMST[v] && key[v]<min){
             min = key[v];
             minIndex = v;
         }
@@ -15,36 +15,36 @@ int minKey(int key[], bool inMST[], int V) {
 }
 
 void printMST(int parent[], int graph[10][10], int V) {
-    cout << "Edge\tWeight\n";
-    for (int i = 1; i < V; i++) {
-        cout << parent[i] << " - " << i << "\t" << graph[i][parent[i]] << endl;
+    cout<<"Edge\tWeight\n";
+    for(int i = 1; i < V; i++){
+        cout<<parent[i]<<" - " <<i<<"\t"<<graph[i][parent[i]]<<endl;
     }
 }
 
 void primMST(int graph[10][10], int V) {
-    int parent[10], key[10];  
-    bool inMST[10];  
+    int parent[10]; // final MST  
+    int key[10];    // cost of edges
+    bool inMST[10]; // check vertexx is visited or not
 
-    for (int i = 0; i < V; i++) {
-        key[i] = INF;
-        inMST[i] = false;
+    for(int i=0;i<V;i++){
+        key[i] =INF;
+        inMST[i]= false;
     }
 
-    key[0] = 0;  
+    key[0] = 0;    
     parent[0] = -1;  
 
-    for (int count = 0; count < V - 1; count++) {
-        int u = minKey(key, inMST, V);
+    for(int count=0;count<V-1;count++){
+       int u = minKey(key, inMST, V);
         inMST[u] = true;
 
-        for (int v = 0; v < V; v++) {
-            if (graph[u][v] && !inMST[v] && graph[u][v] < key[v]) {
-                parent[v] = u;
-                key[v] = graph[u][v];
+        for(int v=0;v<V;v++){
+            if(graph[u][v] && !inMST[v] && graph[u][v]<key[v]){
+                parent[v] =u;
+                key[v] =graph[u][v];
             }
         }
     }
-
     printMST(parent, graph, V);
 }
 
