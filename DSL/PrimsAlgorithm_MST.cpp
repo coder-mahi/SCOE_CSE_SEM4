70ac8df3ea6b8a5070c2cc30103635d594943877
MAHESH <maheshshinde9100@gmail.com>
2025-03-11 20:13:22 +0530

    Prims Algo -- final commit

--- DSL/PrimsAlgorithm_MST.cpp
+++ DSL/PrimsAlgorithm_MST.cpp
@@ -4,52 +4,52 @@ using namespace std;
 void prim(int cost[][10], int v) {
     int mincost = 0;
     int edgeCount = 0;
-    bool visited[10] = {false};
-    visited[0] = true; // Start from the first vertex
+    bool visited[10] ={false};
+    visited[0] =true; // Start from the first vertex
 
-    while (edgeCount < v - 1) {
-        int INF = 99;
+    while(edgeCount < v-1){
+        int INF =99;
         int min = INF;
-        int x = -1;
-        int y = -1;
+        int x= -1;
+        int y =-1;
 
-        for (int p = 0; p < v; p++) {
-            if (visited[p]) {
-                for (int q = 0; q < v; q++) {
-                    if (!visited[q] && cost[p][q] < min) {
-                        min = cost[p][q];
-                        x = p;
-                        y = q;
+        for(int i=0; i<v; i++){
+            if(visited[i]){
+                for(int j = 0; j < v; j++){
+                    if(!visited[j] && cost[i][j] < min){
+                        min = cost[i][j];
+                        x = i;
+                        y = j;
                     }
                 }
             }
         }
 
-        if (x != -1 && y != -1) {
+        if(x!=-1 && y!=-1)
+        {
             visited[y] = true;
-            cout << "Edge (" << x << ", " << y << ") with cost " << min << endl;
-            mincost += min;
+            cout<<"Edge (" <<x<<", "<<y<<") with cost " <<min<<endl;
+            mincost = mincost +min;
             edgeCount++;
         }
     }
 
-    cout << "Total cost of Minimum Spanning Tree: " << mincost << endl;
+    cout<<"Total cost of Minimum Spanning Tree: "<<mincost<<endl;
 }
 
-int main() {
+int main()
+{
     int v;
     cout << "Enter the number of vertices: ";
     cin >> v;
 
     int cost[10][10];
-    cout << "Enter the cost adjacency matrix:" << endl;
-    for (int p = 0; p < v; p++) {
-        for (int q = 0; q < v; q++) {
-            cin >> cost[p][q];
-        }
+    cout<<"Enter the cost adjacency matrix:"<<endl;
+    for(int i=0; i<v; i++){
+        for(int j=0; j<v; j++){
+            cin>>cost[i][j];
+         }
     }
-
-    prim(cost, v);
-
+    prim(cost,v);
     return 0;
 }
