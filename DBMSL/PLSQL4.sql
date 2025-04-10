71300d2232e51adeb1b799e7381df31a216feaaf
MAHESH <maheshshinde9100@gmail.com>
2025-04-10 08:41:16 +0530

    Fix procedure name and refine cursor declaration in copy_proc

--- DBMSL/PLSQL4.sql
+++ DBMSL/PLSQL4.sql
@@ -1,27 +1,20 @@
 delimiter //
-create procedure proc_copy()
+create procedure copy_proc()
 begin
     declare r int;
-    declare n varchar(100);
-    declare c varchar(100);
-    
-    declare vfinished int default 0;
+    declare n varchar(20);
+    declare c varchar(20);
+    declare vfinished int set default 0;
 
-    declare stud_cursor cursor for select rollno1, names1, class1 from student1;
-    declare continue handler for not found set vfinished = 1;
+    declare stud_cur cursor for select (rollno1,names1,class1) from student1;
+    declare continue handler for not found set vfinished 1;
+    open stud_cur;
 
-    open stud_cursor;
-
-    getdata: loop
-        fetch stud_cursor into r, n, c;
-
-        if vfinished = 1 then 
+    getdata:loop
+        fetch stud_cur into (r,n,c);
+        if vfinished = 1 then
             leave getdata;
-        end if;
-
-        insert into student2 (rollno2, names2, class2) values (r, n, c);
+        end if
     end loop getdata;
-    close stud_cursor;
-
-end; 
+end
 //
