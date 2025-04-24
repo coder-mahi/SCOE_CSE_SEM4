df82d21ba36a78f5898d158242473db1cff9bedf
MAHESH <maheshshinde9100@gmail.com>
2025-04-24 17:01:30 +0530

    feat: Add implementation of optimal binary search tree algorithm

---
+++ DSL/DSL_08.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+#define MAX 100
+
+void optimalBST(float p[], float q[], int keys[], int n) {
+    float c[MAX][MAX]={0}; 
+    float w[MAX][MAX] ={0}; 
+    float r[MAX][MAX] ={0}; 
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = i; j <= n; j++) {
+            if (i == j) {
+                w[i][j] = q[i];
+            } else {
+                w[i][j] = w[i][j - 1] + p[j - 1] + q[j];
+            }
+        }
+    }
+
+    for(int i = 0; i <= n; i++) {
+        c[i][i] = 0; 
+    }
+
+    for(int l = 1;l<=n;l++)
+    {
+        for(int i=0;i<=n-l;i++) 
+        {
+            int j = i+l;
+            c[i][j] =9999;
+            for(int k=i+1; k <= j; k++) 
+            { 
+                int cost= c[i][k-1] +c[k][j]+w[i][j];
+                if(cost<c[i][j]) 
+                {
+                    c[i][j] = cost;
+                    r[i][j] = k;
+                }
+            }
+        }
+    }
+
+    cout << "Weight Matrix (w):" <<endl;
+    for(int i=0; i<= n; i++) {
+        for(int j=0; j<=n; j++) 
+        {
+            cout<<w[i][j]<<"\t";
+        }
+        cout << endl;
+    }
+
+    cout << "\nCost Matrix (c):" << endl;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            cout << c[i][j] << "\t";
+        }
+        cout<<endl;
+    }
+
+    cout << "\nRoot Matrix (r):" << endl;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            cout << r[i][j] << "\t";
+        }
+        cout<<endl;
+    }
+
+    cout<<"\nOptimal Cost of the bst :>"<<c[0][n]<<endl;
+}
+
+int main() {
+    int n = 4; 
+    int keys[] = {10, 20, 30,40}; 
+    float p[] = {0.2,0.3,0.4,0.1}; 
+    float q[] = {0.1,0.1,0.2,0.4,0.2}; 
+    optimalBST(p, q, keys, n);
+    return 0;
+}
